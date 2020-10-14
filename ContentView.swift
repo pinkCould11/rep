53d302b12c75838ba0d54bbc5d10795d10388de2
pinkCould11 <72103126+pinkCould11@users.noreply.github.com>
2020-10-14 07:19:32 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  owl
+//
+//  Created by Hana Keinan on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    
+
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .environment(\.sizeCategory, .large)
+            ContentView()
+                .environment(\.sizeCategory, .large)
+        }
+    }
+}
+}
